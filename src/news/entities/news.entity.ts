0846aa8e78f44ab09af7cbcac23cd7f5d6ead6c5
olga <khmarukolgaivanovna@gmail.com>
2024-05-23 10:29:39 +0300

    feat: add entities

--- src/news/entities/news.entity.ts
+++ src/news/entities/news.entity.ts
@@ -1,1 +1,19 @@
-export class News {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'news' })
+export class News {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  title: string;
+
+  @Column({type: "text", nullable: true})
+  description: string;
+
+  @Column({type: "text", nullable: true})
+  text: string;
+
+  @Column({type: "text", nullable: true})
+  img: string;
+}
