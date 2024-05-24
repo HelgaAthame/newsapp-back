b99e957decd0b368e7a89115ed3f491029026931
olga <khmarukolgaivanovna@gmail.com>
2024-05-24 10:15:45 +0300

    feat: add resolvers

--- src/news/news.resolver.ts
+++ src/news/news.resolver.ts
@@ -1,4 +1,42 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
+import { NewsService } from './news.service';
+import { NewsEntity } from './entities/news.entity';
+import { CreateNewsInput } from './inputs/create-news.input';
+import { UpdateNewsInput } from './inputs/update-news.input';
+import {  } from '@nestjs/common';
 
-@Resolver()
-export class NewsResolver {}
+@Resolver('News')
+export class NewsResolver {
+  constructor(private readonly newsService: NewsService) {}
+
+  @Mutation(() => NewsEntity)
+  async createNews(
+    @Args('createNews') createNewsInput: CreateNewsInput,
+  ): Promise<NewsEntity> {
+    return await this.newsService.createNews(createNewsInput);
+  }
+  
+  @Mutation(() => NewsEntity)
+  async updateNews(
+    @Args('updateNews') updateNewsInput: UpdateNewsInput,
+  ): Promise<NewsEntity> {
+    return await this.newsService.updateNews(updateNewsInput);
+  }
+  
+  @Mutation(() => NewsEntity)
+  async removeNews(
+    @Args('id') id: number,
+  ): Promise<number> {
+    return await this.newsService.removeNews(id);
+  }
+
+  @Query(()=>NewsEntity)
+  async getOneNews(@Args('id') id:number): Promise<NewsEntity> {
+    return await this.newsService.getOneNews(id)
+  }
+
+  @Query(()=>[NewsEntity])
+  async getAllNews(): Promise<NewsEntity[]> {
+    return await this.newsService.getAllNews()
+  }
+}
